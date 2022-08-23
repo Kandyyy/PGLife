5e763af0f6b4a9f35ffcd52ca662b411ecfdcf3b
Kandyyy <kingkandy016@gmail.com>
2022-08-23 22:32:03 +0530

    heart icon functionality

--- api/toggle_interested.php
+++ api/toggle_interested.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    include "../includes/database_connect.php";
+    require "../includes/database_connect.php";
 
     if(!isset($_SESSION["user_id"])){
         echo json_encode(array("success" => false, "is_logged_in" => false));
@@ -16,20 +16,7 @@
         echo json_encode(array("success" => false, "msg" => "Something went wrong"));
         return;
     }
-    $table = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    if(count($table) == 0){
-        $sql2 = "INSERT INTO interested_users_properties(property_id, user_id) VALUES ($property_id, $user_id)";
-        $result2 = mysqli_query($conn, $sql2);
-        if(!$result2){
-            echo json_encode(array("success" => false, "msg" => "Something went wrong"));
-            return;
-        }
-        else{
-            echo json_encode(array("success"=>true, "is_interested" = true, "property_id" = $property_id));
-            return;
-        }
-    }
-    else{
+    if(mysqli_num_rows($result) > 0){
         $sql2 = "DELETE FROM interested_users_properties WHERE property_id = $property_id AND user_id = $user_id";
         $result2 = mysqli_query($conn, $sql2);
         if(!$result2){
@@ -37,7 +24,19 @@
             return;
         }
         else{
-            echo json_encode(array("success"=>true, "is_interested" = false, "property_id" = $property_id));
+            echo json_encode(array("success"=>true, "is_interested" => false, "property_id" => $property_id));
+            return;
+        }
+    }
+    else{
+        $sql2 = "INSERT INTO interested_users_properties(property_id, user_id) VALUES ('$property_id', '$user_id')";
+        $result2 = mysqli_query($conn, $sql2);
+        if(!$result2){
+            echo json_encode(array("success" => false, "msg" => "Something went wrong"));
+            return;
+        }
+        else{
+            echo json_encode(array("success"=>true, "is_interested" => true, "property_id" => $property_id));
             return;
         }
     }
